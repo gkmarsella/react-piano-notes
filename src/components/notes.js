e0a8da9f32a95d2c9e8ccb8928a861a092e8e24b
Greg <gkmarsella@gmail.com>
2018-03-16 18:45:46 -0700

    added random changing notes

--- src/components/notes.js
+++ src/components/notes.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
 const Note = ({note}) => {
-	return (<div>{note}</div>);
+
+	return (
+		<div>
+			{note}
+		</div>
+	);
 }
 
 
-const NoteList = ({notes}) => {
-	const notes = ['A','B','C','D','E','F','G','Ab','Bb','Db','Eb'];
-}
+
+export default Note;
--- src/index.js
+++ src/index.js
@@ -2,17 +2,47 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import ReactCountdownClock from 'react-countdown-clock';
 import Piano from './components/piano';
+import Note from './components/notes';
 import './piano.css';
 
+const NUM_NOTES = 11;
+
 class App extends Component {
+
+	constructor(props) {
+		super(props);
+		const notes = Array(NUM_NOTES).fill().map(this.randomNote, this);
+		this.state = {notes};
+
+		setInterval(() => {
+	      	const notes = this.state.notes.slice();
+	      	const randIndex = Math.floor(Math.random() * notes.length);
+	      	notes[randIndex] = this.randomNote();
+	      	this.setState({notes});
+    	}, 300);
+  	}
+
+	randomNote() {
+		let noteIndex = Math.floor(Math.random() * this.props.allNotes.length);
+		return this.props.allNotes[noteIndex];
+	}
+	
 	render() {
+		const notes = this.state.notes.map((note, index) => (
+      		<Note key={index} note={note} />
+    	));
 		return (
 			<div>
+				<div>{notes} </div>
 				<ReactCountdownClock />
 				<Piano />
 			</div>
 		)
 	}
 }
+
+App.defaultProps = {
+	allNotes: ['A','B','C','D','E','F','G','Ab','Bb','Db','Eb']
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
